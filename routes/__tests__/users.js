169045edc53258b053ab45d9bf9dae1881c7c830
Zackery Mowrer <Zackmowrer@gmail.com>
2019-05-26 19:15:20 -0400

    Write test for GET user route

--- routes/__tests__/users.js
+++ routes/__tests__/users.js
@@ -1,6 +1,8 @@
 // Imports
+const bcrypt = require("bcryptjs");
 const request = require("supertest");
 const app = require("../../app");
+
 
 // Test Suite
 describe("/api/users", () => {
@@ -31,6 +33,31 @@ describe("/api/users", () => {
                 
             // Expect a 201 Response
             expect(response.status).toBe(201);
+        });
+    });
+
+    describe("GET method", () => {
+        test("should get a user with the given credentials", async () => {
+            // Set authentication credentials
+            const credentials = {
+                username: emailAddress,
+                password: "password",
+            };
+
+            // GET from API
+            const response = await request(app)
+                .get("/api/users")
+                .auth(credentials.username, credentials.password);
+
+            // Expect a 200 OK response
+            expect(response.status).toBe(200);
+
+            // Get user from response body
+            const user = response.body;
+
+            // Expect user to match email and password data
+            expect(user.emailAddress).toBe(credentials.username);
+            expect(bcrypt.compareSync(credentials.password, user.password)).toBeTruthy();
         });
     });
 });
