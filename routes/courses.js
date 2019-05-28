8a88b0a3246b00271b7912eaff0b0b8132a23d0b
Zackery Mowrer <Zackmowrer@gmail.com>
2019-05-28 07:08:38 -0400

    Fill in GET and POST course routes

--- routes/courses.js
+++ routes/courses.js
@@ -1,24 +1,47 @@
 // Imports
 const express = require("express");
+const authMiddleware = require("../middleware/auth");
+const CourseService = require("../services/CourseService");
+const UserService = require("../services/UserService");
 
 // Express router setup
 const router = express.Router();
 
+// Middleware
+router.use((req, res, next) => {
+    // Provide services on request object
+    req.userService = new UserService();
+    req.courseService = new CourseService();
+
+    // Pass control to next middleware/route
+    next();
+});
+
 // Routes
 router.route("/")
     // GET /api/courses: Get list of courses
-    .get((req, res) => {
-        // TODO: Get courses
+    .get(async (req, res) => {
+        // Get courses
+        const courses = await req.courseService.getList();
         
-        // Respond with "Not Implemented" status and message
-        res.status(501).json({ message: "Not Implemented Yet"});
+        // Respond with list of courses
+        res.json(courses);
     })
     // POST /api/courses: Create new course
-    .post((req, res) => {
-        // TODO: Create course
+    .post(authMiddleware, async (req, res) => {
+        // Define course data
+        const courseData = {
+            title: req.body.title,
+            description: req.body.description,
+            estimatedTime: req.body.estimatedTime,
+            materialsNeeded: req.body.materialsNeeded,
+        };
 
-        // Respond with "Not Implemented" status and message
-        res.status(501).json({ message: "Not Implemented Yet"});
+        // Create course
+        await req.courseService.create(req.user, courseData);
+
+        // Respond with 201 Status
+        res.status(201).end();
     });
 
 router.route("/:id")
