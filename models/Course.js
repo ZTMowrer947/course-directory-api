54276f6ee0930d872b9fba1392880ff122538ec5
Zackery Mowrer <Zackmowrer@gmail.com>
2019-05-29 16:09:05 +0000

    Add model validation to Course model

--- models/Course.js
+++ models/Course.js
@@ -19,18 +19,52 @@ Course.init({
     title: {
         type: Sequelize.STRING(127),
         allowNull: false,
+        validate: {
+            // Field mustn't be null
+            notNull: {
+                msg: "title is a required field.",
+            },
+
+            // Field mustn't be empty
+            notEmpty: {
+                msg: "title is a required field",
+            },
+        },
     },
     description: {
         type: Sequelize.TEXT,
         allowNull: false,
+        validate: {
+            // Field mustn't be null
+            notNull: {
+                msg: "description is a required field.",
+            },
+
+            // Field mustn't be empty
+            notEmpty: {
+                msg: "description is a required field",
+            },
+        },
     },
     estimatedTime: {
         type: Sequelize.STRING,
         allowNull: true,
+        validate: {
+            // Field mustn't be empty
+            notEmpty: {
+                msg: "estimated time must not be empty",
+            },
+        },
     },
     materialsNeeded: {
         type: Sequelize.STRING,
         allowNull: true,
+        validate: {
+            // Field mustn't be empty
+            notEmpty: {
+                msg: "materials needed must not be empty",
+            },
+        },
     },
 }, {
     sequelize,
