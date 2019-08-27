b27401a77aa6e8bb11d93b8e834ec97f73251d49
Zackery Mowrer <Zackmowrer@gmail.com>
2019-08-27 06:22:11 -0400

    Add validation decorators to UserModifyDTO

--- src/models/UserModifyDTO.ts
+++ src/models/UserModifyDTO.ts
@@ -1,13 +1,45 @@
 // Imports
+import {
+    Matches,
+    IsNotEmpty,
+    IsEmail,
+    MaxLength,
+    MinLength,
+} from "class-validator";
 import User from "../database/entities/User.entity";
 
 // DTO
 export default class UserModifyDTO implements Partial<User> {
+    @IsNotEmpty({ message: "firstName is a required field." })
+    @Matches(/^[A-Za-z0-9].,' $/, {
+        message:
+            "firstName must only consist of alphanumeric characters and punctuation",
+    })
+    @MaxLength(96, {
+        message: "firstName must be no more than 96 characters long.",
+    })
     public firstName!: string;
 
+    @IsNotEmpty({ message: "lastName is a required field." })
+    @Matches(/^[A-Za-z0-9].,' $/, {
+        message:
+            "lastName must only consist of alphanumeric characters and punctuation",
+    })
+    @MaxLength(96, {
+        message: "lastName must be no more than 96 characters long.",
+    })
     public lastName!: string;
 
+    @IsNotEmpty({ message: "emailAddress is a required field." })
+    @IsEmail(undefined, {
+        message: "emailAddress must be a valid email address.",
+    })
+    @MaxLength(127, {
+        message: "emailAddress must be no more than 127 characters long.",
+    })
     public emailAddress!: string;
 
+    @IsNotEmpty({ message: "password is a required field." })
+    @MinLength(8, { message: "password must be at least 8 characters long." })
     public password!: string;
 }
