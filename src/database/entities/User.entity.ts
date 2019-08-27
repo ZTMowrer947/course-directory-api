10a2e4d8486ec8345451dff944ec5945fc6ab63f
Zackery Mowrer <Zackmowrer@gmail.com>
2019-08-26 20:11:53 -0400

    Update user entity to hash passwords with argon2

--- src/database/entities/User.entity.ts
+++ src/database/entities/User.entity.ts
@@ -1,5 +1,14 @@
 // Imports
-import { Column, Entity, OneToMany } from "typeorm";
+import os from "os";
+import argon2 from "argon2";
+import {
+    Column,
+    Entity,
+    OneToMany,
+    AfterLoad,
+    BeforeInsert,
+    BeforeUpdate,
+} from "typeorm";
 import TimestampedEntity from "./TimestampedEntity";
 import Course from "./Course.entity";
 
@@ -15,9 +24,31 @@ export default class User extends TimestampedEntity {
     @Column({ length: 127, nullable: false, unique: true })
     public emailAddress!: string;
 
+    private tempPassword!: string;
+
     @Column({ nullable: false })
     public password!: string;
 
     @OneToMany(() => Course, course => course.creator)
     public createdCourses!: Course[];
+
+    @AfterLoad()
+    private loadTempPassword(): void {
+        this.tempPassword = this.password;
+    }
+
+    @BeforeInsert()
+    private async hashPassword(): Promise<void> {
+        this.password = await argon2.hash(this.password, {
+            type: argon2.argon2id,
+            hashLength: 48,
+            parallelism: os.cpus.length > 2 ? os.cpus.length - 2 : 1,
+            memoryCost: 2 ** 16,
+        });
+    }
+
+    @BeforeUpdate()
+    private async hashPasswordIfUpdated(): Promise<void> {
+        if (this.password !== this.tempPassword) await this.hashPassword();
+    }
 }
