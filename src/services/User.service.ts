bc171f6779155f24e1e3ec465a50288147ed46a6
Zackery Mowrer <Zackmowrer@gmail.com>
2019-08-26 20:53:42 -0400

    Rewrite User service

---
+++ src/services/User.service.ts
@@ -0,0 +1,50 @@
+// Imports
+import { Service } from "typedi";
+import { Repository } from "typeorm";
+import { InjectRepository } from "typeorm-typedi-extensions";
+import User from "../database/entities/User.entity";
+import UserModifyDTO from "../models/UserModifyDTO";
+
+// Service
+@Service()
+export default class UserService {
+    private repository: Repository<User>;
+
+    public constructor(@InjectRepository(User) repository: Repository<User>) {
+        this.repository = repository;
+    }
+
+    public async getUserByEmail(emailAddress: string): Promise<User> {
+        // Create query
+        const query = this.repository
+            .createQueryBuilder("users")
+            .where("user.emailAddress = :emailAddress", { emailAddress });
+
+        // Execute query
+        const user = await query.getOne();
+
+        // If user was not found, throw an error
+        if (!user) {
+            throw new Error(
+                `User not found with email address "${emailAddress}".`
+            );
+        }
+
+        // Otherwise, return the user
+        return user;
+    }
+
+    public async create(userData: UserModifyDTO): Promise<void> {
+        // Create user instance
+        const user = new User();
+
+        // Set user properties
+        user.firstName = userData.firstName;
+        user.lastName = userData.lastName;
+        user.emailAddress = userData.emailAddress;
+        user.password = userData.password;
+
+        // Persist user to database
+        await this.repository.save(user);
+    }
+}
