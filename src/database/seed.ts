b924223213017008be69824515f8fa0573aaffac
Zackery Mowrer <Zackmowrer@gmail.com>
2019-09-23 19:06:23 -0400

    Modify seed script to use class-transformer

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -1,4 +1,5 @@
 // Imports
+import { plainToClass } from "class-transformer";
 import { Connection } from "typeorm";
 import User from "./entities/User.entity";
 import Course from "./entities/Course.entity";
@@ -42,21 +43,7 @@ const seed = async (connection: Connection): Promise<void> => {
         ];
 
         // Create users
-        const users = userSeedData.map(userData => {
-            // Create user instance
-            const user = new User();
-
-            // Set user properties
-            const { firstName, lastName, emailAddress, password } = userData;
-
-            user.firstName = firstName;
-            user.lastName = lastName;
-            user.emailAddress = emailAddress;
-            user.password = password;
-
-            // Return the user instance
-            return user;
-        });
+        const users = plainToClass(User, userSeedData);
 
         // Persist users to database
         await entityManager.save(users);
@@ -64,7 +51,7 @@ const seed = async (connection: Connection): Promise<void> => {
         // Define course seed data
         const courseSeedData = [
             {
-                userIndex: 0,
+                creator: users[0],
                 title: "Build a Basic Bookcase",
                 description:
                     "High-end furniture projects are great to dream about. But unless you have a well-equipped shop and some serious woodworking experience to draw on, it can be difficult to turn the dream into a reality.\n\nNot every piece of furniture needs to be a museum showpiece, though. Often a simple design does the job just as well and the experience gained in completing it goes a long way toward making the next project even better.\n\nOur pine bookcase, for example, features simple construction and it's designed to be built with basic woodworking tools. Yet, the finished project is a worthy and useful addition to any room of the house. While it's meant to rest on the floor, you can convert the bookcase to a wall-mounted storage unit by leaving off the baseboard. You can secure the cabinet to the wall by screwing through the cabinet cleats into the wall studs.\n\nWe made the case out of materials available at most building-supply dealers and lumberyards, including 1/2 x 3/4-in. parting strip, 1 x 2, 1 x 4 and 1 x 10 common pine and 1/4-in.-thick lauan plywood. Assembly is quick and easy with glue and nails, and when you're done with construction you have the option of a painted or clear finish.\n\nAs for basic tools, you'll need a portable circular saw, hammer, block plane, combination square, tape measure, metal rule, two clamps, nail set and putty knife. Other supplies include glue, nails, sandpaper, wood filler and varnish or paint and shellac.\n\nThe specifications that follow will produce a bookcase with overall dimensions of 10 3/4 in. deep x 34 in. wide x 48 in. tall. While the depth of the case is directly tied to the 1 x 10 stock, you can vary the height, width and shelf spacing to suit your needs. Keep in mind, though, that extending the width of the cabinet may require the addition of central shelf supports.",
@@ -73,7 +60,7 @@ const seed = async (connection: Connection): Promise<void> => {
                     "* 1/2 x 3/4 inch parting strip\n* 1 x 2 common pine\n* 1 x 4 common pine\n* 1 x 10 common pine\n* 1/4 inch thick lauan plywood\n* Finishing Nails\n* Sandpaper\n* Wood Glue\n* Wood Filler\n* Minwax Oil Based Polyurethane\n",
             },
             {
-                userIndex: 1,
+                creator: users[1],
                 title: "Learn How to Program",
                 description:
                     "In this course, you'll learn how to write code like a pro!",
@@ -82,7 +69,7 @@ const seed = async (connection: Connection): Promise<void> => {
                     "* Notebook computer running Mac OS X or Windows\n* Text editor",
             },
             {
-                userIndex: 1,
+                creator: users[1],
                 title: "Learn How to Test Programs",
                 description:
                     "In this course, you'll learn how to test programs.",
@@ -90,29 +77,7 @@ const seed = async (connection: Connection): Promise<void> => {
         ];
 
         // Create courses
-        const courses = courseSeedData.map(courseData => {
-            // Create course instance
-            const course = new Course();
-
-            // Set course properties
-            const {
-                userIndex,
-                title,
-                description,
-                estimatedTime,
-                materialsNeeded,
-            } = courseData;
-            const user = users[userIndex];
-
-            course.title = title;
-            course.description = description;
-            course.estimatedTime = estimatedTime || null;
-            course.materialsNeeded = materialsNeeded || null;
-            course.creator = user;
-
-            // Return course instance
-            return course;
-        });
+        const courses = plainToClass(Course, courseSeedData);
 
         // Persist courses to database
         await entityManager.save(courses);
