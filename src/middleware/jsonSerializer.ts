b8535ebf1c4a5ebffde2775bc82c1e9b0da9d156
Zackery Mowrer <Zackmowrer@gmail.com>
2019-09-24 19:04:54 -0400

    Modify JSON serializer to handle undefined

--- src/middleware/jsonSerializer.ts
+++ src/middleware/jsonSerializer.ts
@@ -7,8 +7,8 @@ const jsonSerializer: Middleware = async (ctx, next) => {
     // Proceed with middleware chain
     await next();
 
-    // If the context body is JSON-serializable,
-    if (isJSONSerializable(ctx.body)) {
+    // If the context body is defined and is JSON-serializable,
+    if (ctx.body && isJSONSerializable(ctx.body)) {
         // Serialize it as JSON
         ctx.body = ctx.body.toJSON();
     }
