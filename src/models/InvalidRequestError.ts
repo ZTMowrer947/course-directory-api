bf2c6e5bf867ccb58c8dce797641415cb39c048a
Zackery Mowrer <Zackmowrer@gmail.com>
2019-09-24 18:40:56 -0400

    Add InvalidRequestError type

---
+++ src/models/InvalidRequestError.ts
@@ -0,0 +1,21 @@
+// Imports
+import { classToPlain } from "class-transformer";
+import { ValidationError } from "class-validator";
+import AppError from "./AppError";
+
+// Error type
+export default class InvalidRequestError extends AppError {
+    public readonly validationErrors: ValidationError[];
+
+    public constructor(validationErrors: ValidationError[] = []) {
+        super(undefined, 401);
+
+        this.validationErrors = validationErrors;
+    }
+
+    public toJSON(): object {
+        return {
+            errors: classToPlain(this.validationErrors),
+        };
+    }
+}
