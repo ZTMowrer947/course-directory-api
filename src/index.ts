baeb6cf3507d1ad08832c02f21bd8df188e82622
Zackery Mowrer <Zackmowrer@gmail.com>
2019-09-24 18:36:36 -0400

    Configure app to get port to listen on from env

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,13 @@ ormBootstrap().then(() => {
     // Create HTTP server
     const server = http.createServer(app.callback());
 
+    // Get port to listen on
+    const port = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+
     // Listen on given port
-    server.listen(8000);
+    server.listen(port);
 
     server.once("listening", () => {
-        console.log("Koa server now running on port 8000...");
+        console.log(`Koa server now running on port ${port}...`);
     });
 });
