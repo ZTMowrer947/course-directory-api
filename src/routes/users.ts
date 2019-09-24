dddb881d50fdb24d9aec649f5363af5383682a42
Zackery Mowrer <Zackmowrer@gmail.com>
2019-09-24 18:35:19 -0400

    Re-implement user GET route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,15 @@
 // Imports
+import { ParameterizedContext } from "koa";
 import Router from "koa-router";
+import auth from "../middleware/auth";
+import AuthState from "../models/AuthState";
 
 // Router setup
 const userRouter = new Router();
 
 // Routes
-userRouter.get("/", ctx => {
-    ctx.body = { message: "User endpoint" };
+userRouter.get("/", auth, async (ctx: ParameterizedContext<AuthState>) => {
+    ctx.body = ctx.state.user;
 });
 
 // Export
