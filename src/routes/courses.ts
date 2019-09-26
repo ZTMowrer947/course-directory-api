2af11f52913803c93a9defce24431fbf4cff0a5b
Zackery Mowrer <Zackmowrer@gmail.com>
2019-09-26 19:17:44 -0400

    Add Course POST, PUT, and DELETE routes

--- src/routes/courses.ts
+++ src/routes/courses.ts
@@ -1,10 +1,17 @@
 // Imports
-import { ParameterizedContext } from "koa";
+import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
+import { ParameterizedContext, Middleware } from "koa";
 import Router from "koa-router";
 import { Container } from "typedi";
 import CourseState from "../models/CourseState";
 import CourseService from "../services/Course.service";
 import CourseByIdState from "../models/CourseByIdState";
+import AuthState from "../models/AuthState";
+import auth from "../middleware/auth";
+import CourseModifyDTO from "../models/CourseModifyDTO";
+import InvalidRequestError from "../models/InvalidRequestError";
+import AppError from "../models/AppError";
 
 // Router setup
 const courseRouter = new Router();
@@ -18,17 +25,18 @@ courseRouter.use(async (ctx: ParameterizedContext<CourseState>, next) => {
     await next();
 });
 
-// Param handlers
-courseRouter.param(
-    "id",
-    async (id, ctx: ParameterizedContext<CourseByIdState>, next) => {
-        // Find course by ID and attach to context state
-        ctx.state.course = await ctx.state.courseService.getCourseById(id);
+const courseById: Middleware<CourseByIdState, Router.IRouterContext> = async (
+    ctx,
+    next
+) => {
+    // Find course by ID and attach to context state
+    ctx.state.course = await ctx.state.courseService.getCourseById(
+        ctx.params.id
+    );
 
-        // Continue middleware flow
-        await next();
-    }
-);
+    // Continue middleware flow
+    await next();
+};
 
 // Routes
 courseRouter.get("/", async (ctx: ParameterizedContext<CourseState>) => {
@@ -36,10 +44,105 @@ courseRouter.get("/", async (ctx: ParameterizedContext<CourseState>) => {
     ctx.body = await ctx.state.courseService.getList();
 });
 
-courseRouter.get("/:id", async (ctx: ParameterizedContext<CourseByIdState>) => {
-    // Response with course found by ID
-    ctx.body = ctx.state.course;
-});
+courseRouter.post(
+    "/",
+    auth,
+    async (ctx: ParameterizedContext<CourseState & AuthState>) => {
+        // Transform request body into CourseModifyDTO
+        const courseData = plainToClass(CourseModifyDTO, ctx.request.body, {
+            excludeExtraneousValues: true,
+        });
+
+        // Validate course data
+        const errors = await validate(courseData);
+
+        // If errors were found, throw validation error
+        if (errors.length > 0) {
+            throw new InvalidRequestError(errors);
+        }
+
+        // Create course and get ID
+        const id = await ctx.state.courseService.create(
+            ctx.state.user,
+            courseData
+        );
+
+        // Get base URL
+        const baseURL = `${ctx.request.protocol}://${ctx.request.host}`;
+
+        // Set Location header
+        ctx.set("Location", `${baseURL}/api/courses/${id}`);
+
+        // Set status to 201
+        ctx.status = 201;
+    }
+);
+
+courseRouter.get(
+    "/:id",
+    courseById,
+    async (ctx: ParameterizedContext<CourseByIdState>) => {
+        // Response with course found by ID
+        ctx.body = ctx.state.course;
+    }
+);
+
+courseRouter.put(
+    "/:id",
+    auth,
+    courseById,
+    async (ctx: ParameterizedContext<CourseByIdState & AuthState>) => {
+        // If course does not belong to authenticated user,
+        if (ctx.state.course.creator.id !== ctx.state.user.id) {
+            // Throw 403 error
+            throw new AppError(
+                "You are not allowed to modify this resource.",
+                403
+            );
+        }
+
+        // Otherwise, ransform request body into CourseModifyDTO
+        const updateData = plainToClass(CourseModifyDTO, ctx.request.body, {
+            excludeExtraneousValues: true,
+        });
+
+        // Validate course data
+        const errors = await validate(updateData);
+
+        // If errors were found, throw validation error
+        if (errors.length > 0) {
+            throw new InvalidRequestError(errors);
+        }
+
+        // Otherwise, update course
+        await ctx.state.courseService.update(ctx.state.course, updateData);
+
+        // Set status to 204
+        ctx.status = 204;
+    }
+);
+
+courseRouter.delete(
+    "/:id",
+    auth,
+    courseById,
+    async (ctx: ParameterizedContext<CourseByIdState & AuthState>) => {
+        // If course does not belong to authenticated user,
+        if (ctx.state.course.creator.id !== ctx.state.user.id) {
+            // Throw 403 error
+            throw new AppError(
+                "You are not allowed to modify this resource.",
+                403
+            );
+        }
+
+        // Otherwise, delete course
+        await ctx.state.courseService.delete(ctx.state.course);
+
+        // Set status to 204
+        ctx.status = 204;
+    }
+);
 
 // Export
 export default courseRouter;
