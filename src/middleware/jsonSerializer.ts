534711a6a91f318045578297f09e551ce5eb4333
Zackery Mowrer <Zackmowrer@gmail.com>
2019-09-26 18:37:37 -0400

    Modify serializer to operate on arrays

--- src/middleware/jsonSerializer.ts
+++ src/middleware/jsonSerializer.ts
@@ -7,10 +7,21 @@ const jsonSerializer: Middleware = async (ctx, next) => {
     // Proceed with middleware chain
     await next();
 
-    // If the context body is defined and is JSON-serializable,
-    if (ctx.body && isJSONSerializable(ctx.body)) {
-        // Serialize it as JSON
-        ctx.body = ctx.body.toJSON();
+    // If the context body is defined,
+    if (ctx.body) {
+        // If the context body is an array, and all elements are JSON-Serializable
+        if (
+            Array.isArray(ctx.body) &&
+            ctx.body.every(item => isJSONSerializable(item))
+        ) {
+            // Map all items into JSON
+            ctx.body = ctx.body.map(item => item.toJSON());
+        } else if (isJSONSerializable(ctx.body)) {
+            // Otherwise, if the body is JSON-Serializable,
+
+            // Serialize it as JSON
+            ctx.body = ctx.body.toJSON();
+        }
     }
 
     // Otherwise, leave it unchanged
