f4723fc2b96e8b1076d612fe738b2da1c34f99f1
Zackery Mowrer <Zackmowrer@gmail.com>
2019-11-26 13:19:59 -0500

    Add TypeGraphQL decorators to entities

--- src/database/entities/Course.entity.ts
+++ src/database/entities/Course.entity.ts
@@ -1,26 +1,33 @@
 // Imports
 import { classToPlain, Expose, Type } from "class-transformer";
 import { Column, Entity, ManyToOne } from "typeorm";
+import { ObjectType, Field } from "type-graphql";
+import EntityBase from "./EntityBase";
 import TimestampedEntity from "./TimestampedEntity";
 import User from "./User.entity";
 
 // Entity
 @Entity("courses")
+@ObjectType({ implements: EntityBase })
 export default class Course extends TimestampedEntity {
     @Expose()
     @Column({ length: 127, nullable: false })
+    @Field({ nullable: false })
     public title!: string;
 
     @Expose()
     @Column({ type: "text", nullable: false })
+    @Field({ nullable: false })
     public description!: string;
 
     @Expose()
     @Column({ nullable: true, default: null })
+    @Field({ nullable: true })
     public estimatedTime!: string | null;
 
     @Expose()
     @Column({ nullable: true, default: null })
+    @Field({ nullable: true })
     public materialsNeeded!: string | null;
 
     @Expose()
@@ -29,6 +36,7 @@ export default class Course extends TimestampedEntity {
         nullable: false,
         onDelete: "CASCADE",
     })
+    @Field(() => User, { nullable: false })
     public creator!: User;
 
     /* istanbul ignore next */
