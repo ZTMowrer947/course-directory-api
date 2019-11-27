cfc2da5789b3c92f7743b99c9a1736c33d79d189
Zackery Mowrer <Zackmowrer@gmail.com>
2019-11-27 07:23:00 -0500

    Rewrite authChecker to attach user to context

--- src/authChecker.ts
+++ src/authChecker.ts
@@ -2,7 +2,8 @@
 import argon2 from "argon2";
 import basicAuth from "basic-auth";
 import { Container } from "typedi";
-import { AuthChecker } from "type-graphql";
+import { AuthChecker, Ctx } from "type-graphql";
+import AppError from "./models/AppError";
 import GraphQLContext from "./models/GraphQLContext";
 import UserService from "./services/User.service";
 
@@ -14,16 +15,33 @@ const authChecker: AuthChecker<GraphQLContext> = async ({ context }) => {
     // Get credentials
     const credentials = basicAuth(context.koaCtx.req);
 
-    // Attempt to find user using email
-    const user =
-        credentials && (await userService.getUserByEmail(credentials.name));
+    // If credentials were not found, throw error
+    if (!credentials) {
+        context.koaCtx.set("WWW-Authenticate", "Basic");
 
-    // Grant access if credentials were extracted, user exists, and password matches
-    return (
-        !!credentials &&
-        !!user &&
-        (await argon2.verify(user.password, credentials.pass))
-    );
+        throw new AppError(
+            "Credentials are required to perform the requested action.",
+            401
+        );
+    }
+
+    // Attempt to find user using email
+    const user = await userService.getUserByEmail(credentials.name);
+
+    // If user was not found, deny access
+    if (!user) return false;
+
+    // Otherwise, check if correct password was supplied
+    const validPassword = await argon2.verify(user.password, credentials.pass);
+
+    // If not, deny access
+    if (!validPassword) return false;
+
+    // Otherwise, attach user to context
+    context.user = user;
+
+    // Grant access
+    return true;
 };
 
 // Export
