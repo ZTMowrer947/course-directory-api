3e35c06aff1b5020f8c1e8c9706b7a7fb65c6e9d
Zackery Mowrer <Zackmowrer@gmail.com>
2020-12-21 20:42:40 -0500

    Add ormconfig

---
+++ ormconfig.js
@@ -0,0 +1,43 @@
+// Imports
+const path = require('path');
+
+// Paths
+const rootDir = process.cwd();
+const srcDir = path.join(rootDir, 'src');
+const distDir = path.join(rootDir, 'dist');
+
+/**
+ * @type {import("typeorm").ConnectionOptions[]}
+ */
+module.exports = [
+  {
+    name: 'development',
+    type: 'sqlite',
+    database: path.join(rootDir, 'coursedir-dev.sqlite'),
+    entities: [path.join(srcDir, 'entities', '**.ts')],
+    migrations: [path.join(srcDir, 'migrations', '**.ts')],
+    cli: {
+      entitiesDir: path.join('src', 'entities'),
+      migrationsDir: path.join('src', 'migrations'),
+    },
+  },
+  {
+    name: 'testing',
+    type: 'sqlite',
+    database: ':memory:',
+    entities: [path.join(srcDir, 'entities', '**.ts')],
+    migrations: [path.join(srcDir, 'migrations', '**.ts')],
+    migrationsRun: true,
+  },
+  {
+    name: 'production',
+    type: 'sqlite',
+    database: path.join(rootDir, 'coursedir-prod.sqlite'),
+    entities: [path.join(distDir, 'entities', '**.js')],
+    migrations: [path.join(distDir, 'migrations', '**.js')],
+    cli: {
+      entitiesDir: path.join('dist', 'entities'),
+      migrationsDir: path.join('dist', 'migrations'),
+    },
+  },
+];
