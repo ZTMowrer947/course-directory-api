8a5430fa05806fa8aadfb089d58f56917f3298af
Zackery Mowrer <zackm2401@protonmail.com>
2020-12-27 08:36:37 -0500

    Add validators to User DTO

--- src/dto/user.ts
+++ src/dto/user.ts
@@ -1,15 +1,33 @@
 // Imports
-import User from '../entities/user.entity';
+import { Exclude, Expose } from 'class-transformer';
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+
+import User from '@/entities/user.entity';
+
 import DtoBase from './base';
 
 // User DTO
+@Exclude()
 class UserDto extends DtoBase<User> implements Partial<User> {
+  @Expose()
+  @IsNotEmpty({ message: 'First Name is required' })
   firstName!: string;
 
+  @Expose()
+  @IsNotEmpty({ message: 'Last Name is required' })
   lastName!: string;
 
+  @Expose()
+  @IsNotEmpty({ message: 'Email Address is required' })
+  @IsEmail(undefined, { message: 'Email Address must be a valid email' })
+  @MaxLength(127, {
+    message: 'Email Address must be no more than 127 characters long',
+  })
   emailAddress!: string;
 
+  @Expose()
+  @IsNotEmpty({ message: 'Password is required' })
+  @MinLength(12, { message: 'Password must be at least 12 characters long' })
   password!: string;
 
   constructor(user?: User) {
