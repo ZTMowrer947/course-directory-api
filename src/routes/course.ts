420afdb27cf48715ac1b8f47173678b047aec505
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2021-08-22 21:54:42 -0400

    Add basic GET course route

--- src/routes/course.ts
+++ src/routes/course.ts
@@ -18,4 +18,42 @@ courseRouter.get('/', async (ctx) => {
   ctx.body = courses;
 });
 
+courseRouter.get('/:id', async (ctx) => {
+  const idString = ctx.params['id'];
+
+  if (!idString || Number.isNaN(Number.parseInt(idString, 10))) {
+    ctx.throw(400, 'id must be numeric');
+  } else {
+    const id = Number.parseInt(idString, 10);
+
+    const prisma = new PrismaClient();
+
+    const course = await prisma.course.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        estimatedTime: true,
+        materialsNeeded: true,
+        userId: true,
+        user: {
+          select: {
+            firstName: true,
+            lastName: true,
+          },
+        },
+      },
+    });
+
+    if (!course) {
+      ctx.throw(404, 'course not found');
+    } else {
+      ctx.body = course;
+    }
+  }
+});
+
 export default courseRouter;
