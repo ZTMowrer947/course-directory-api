0d2de71d29d802b4dc4e2a070215b663bac26d49
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2021-09-11 12:55:36 -0400

    Add course POST route

--- src/routes/course.ts
+++ src/routes/course.ts
@@ -1,8 +1,11 @@
 import Router from '@koa/router';
 
+import auth, { AuthState } from '../middleware/auth';
 import prismaMiddleware, { PrismaState } from '../middleware/prisma';
+import validateBody from '../middleware/validate-body';
+import CourseSchema, { CourseInput } from '../validation/course';
 
-const courseRouter = new Router<PrismaState>({
+const courseRouter = new Router<PrismaState & AuthState>({
   prefix: '/api/courses',
 });
 
@@ -60,6 +63,49 @@ courseRouter.get(
     } else {
       ctx.body = course;
     }
+  }
+);
+
+courseRouter.post(
+  '/',
+  prismaMiddleware,
+  auth,
+  validateBody(CourseSchema),
+  async (ctx) => {
+    const courseData = ctx.request.body as CourseInput;
+
+    const createdCourse = await ctx.state.prisma.course.create({
+      data: {
+        title: courseData.title,
+        description: courseData.description,
+        estimatedTime: courseData.estimatedTime,
+        materialsNeeded: courseData.materialsNeeded,
+        user: {
+          connect: {
+            id: ctx.state.user.id,
+          },
+        },
+      },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        estimatedTime: true,
+        materialsNeeded: true,
+        userId: true,
+        user: {
+          select: {
+            firstName: true,
+            lastName: true,
+          },
+        },
+      },
+    });
+
+    ctx.status = 201;
+    ctx.set('Location', `/api/courses/${createdCourse.id}`);
+
+    ctx.body = createdCourse;
   }
 );
 
