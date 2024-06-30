034bdc85fc309b4f883379a533c3cfde439b13e4
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-06-30 12:53:27 -0500

    Remove link to old Koa app

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,10 @@
-import { createApp, fromNodeMiddleware } from 'h3';
+import { createApp } from 'h3';
 
-import koaApp from './koa/app.ts';
 import routes from './routes.ts';
 
 const app = createApp();
 
 // Handle routing
 app.use(routes);
-
-// Send other incoming requests to Koa app for now
-app.use(fromNodeMiddleware(koaApp.callback()));
 
 export default app;
