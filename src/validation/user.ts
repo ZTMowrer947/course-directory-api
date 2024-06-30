589421f4b2766485721728084eb250e2df13953e
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-06-30 17:19:26 -0500

    Add user input schema

---
+++ src/validation/user.ts
@@ -0,0 +1,22 @@
+import { validate as validateEmail } from 'email-validator-rfc-5322';
+import {
+  type Infer,
+  nonempty,
+  object,
+  refine,
+  size,
+  string,
+} from 'superstruct';
+
+const Email = refine(
+  nonempty(string()),
+  'email',
+  (value) => validateEmail(value) || 'input value must be a valid email'
+);
+
+export const UserInput = object({
+  emailAddress: Email,
+  password: size(string(), 8, Infinity),
+});
+
+export type UserInputData = Infer<typeof UserInput>;
