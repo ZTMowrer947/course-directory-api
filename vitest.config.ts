7ec020b7fd11e7baf3178af11b9da36045d04112
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-02 09:37:11 -0500

    Add and setup vitest

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import tsPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [tsPaths()],
+  test: {
+    include: ['tests/**/*.{spec,test}.?(c|m)[jt]s?(x)'],
+  },
+});
