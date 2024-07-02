4cf5ff90c30d94671f6c0f5caf3291891995f7eb
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-02 13:03:03 -0500

    Update course list test to pressed db with fake data

--- tests/integration/smoke.spec.ts
+++ tests/integration/smoke.spec.ts
@@ -1,3 +1,6 @@
+import { faker } from '@faker-js/faker';
+import type { Prisma } from '@prisma/client';
+import argon2 from 'argon2';
 import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
 
 import { getAppHandler, prismaMock } from './utils.ts';
@@ -14,6 +17,46 @@ describe('Integration API tests', () => {
   });
 
   test('GET /api/courses retrieves course listing', async () => {
+    // Seed database with test data
+    const { prisma } = await import('~/prisma-client.ts');
+
+    const [firstName, lastName] = [
+      faker.person.firstName(),
+      faker.person.lastName(),
+    ];
+    const userData = {
+      firstName,
+      lastName,
+      emailAddress: faker.internet.email({ firstName, lastName }),
+      password: await argon2.hash(faker.internet.password({ length: 12 })),
+    } satisfies Prisma.UserCreateWithoutCoursesInput;
+
+    const courseData = Array.from({ length: 2 }, () => {
+      return {
+        title: faker.lorem.words(3),
+        description: faker.lorem.paragraph(),
+      } satisfies Prisma.CourseCreateManyUserInput;
+    });
+
+    const { courses: expectedCourses } = await prisma.user.create({
+      data: {
+        ...userData,
+        courses: {
+          createMany: {
+            data: courseData,
+          },
+        },
+      },
+      select: {
+        courses: {
+          select: {
+            id: true,
+            title: true,
+          },
+        },
+      },
+    });
+
     // Setup web handler
     const handler = await getAppHandler();
 
@@ -25,6 +68,6 @@ describe('Integration API tests', () => {
     // Expect a successful JSON response with the correct course listing
     expect(res.ok).toBe(true);
     expect(res.headers.get('Content-Type')).toBe('application/json');
-    await expect(res.json()).resolves.toStrictEqual([]);
+    await expect(res.json()).resolves.toStrictEqual(expectedCourses);
   });
 });
