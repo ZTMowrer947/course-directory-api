968763f7001ace88fe8aab8ccfa971f29addc5b4
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-07-02 14:24:35 -0500

    Add tests for course detail fetch route

--- tests/integration/course-routes.spec.ts
+++ tests/integration/course-routes.spec.ts
@@ -99,4 +99,94 @@ describe('Integration API tests', () => {
     expect(res.headers.get('Content-Type')).toBe('application/json');
     await expect(res.json()).resolves.toStrictEqual(expectedCourses);
   });
+
+  test('GET /api/courses/:id retrieves full course details for courses that exist', async () => {
+    // Seed database with test data
+    const { prisma } = await import('~/prisma-client.ts');
+
+    const [firstName, lastName] = [
+      faker.person.firstName(),
+      faker.person.lastName(),
+    ];
+    const userData = {
+      firstName,
+      lastName,
+      emailAddress: faker.internet.email({ firstName, lastName }),
+      password: await argon2.hash(faker.internet.password({ length: 12 })),
+    } satisfies Prisma.UserCreateWithoutCoursesInput;
+
+    const courseData = Array.from({ length: 2 }, (_, index) => {
+      return {
+        title: faker.lorem.words(3),
+        description: faker.lorem.paragraph(),
+        estimatedTime:
+          index % 2 === 0
+            ? null
+            : `${faker.number.int({ min: 2, max: 10 })} hours`,
+        materialsNeeded:
+          index % 2 === 0
+            ? null
+            : Array.from({ length: 3 }, () => `- ${faker.lorem.words(3)}`).join(
+                '\n'
+              ),
+      } satisfies Prisma.CourseCreateManyUserInput;
+    });
+
+    const { courses } = await prisma.user.create({
+      data: {
+        ...userData,
+        courses: {
+          createMany: {
+            data: courseData,
+          },
+        },
+      },
+      select: {
+        courses: {
+          select: {
+            id: true,
+            title: true,
+            description: true,
+            estimatedTime: true,
+            materialsNeeded: true,
+            userId: true,
+            user: {
+              select: {
+                firstName: true,
+                lastName: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const handler = await getAppHandler();
+
+    // Get course details for each created course
+    for (const course of courses) {
+      const url = new URL(
+        `/api/courses/${encodeURIComponent(course.id)}`,
+        'http://localhost:3000'
+      );
+
+      const res = await handler(new Request(url));
+
+      // Expect each course request to result in a successful JSON response with the correct data
+      expect(res.ok).toBe(true);
+      expect(res.headers.get('Content-Type')).toBe('application/json');
+      await expect(res.json()).resolves.toStrictEqual(course);
+    }
+  });
+
+  test('GET /api/courses/:id returns 404 for a nonexistent course', async () => {
+    // Request the data for a course without any existing in the database
+    const handler = await getAppHandler();
+
+    const url = new URL('/api/courses/1', 'http://localhost:5000');
+    const res = await handler(new Request(url));
+
+    expect(res.ok).toBe(false);
+    expect(res.status).toBe(404);
+  });
 });
