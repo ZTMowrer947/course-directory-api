1fb9e9249487eae2b767eae04d9176cfab81abe3
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2024-08-09 06:42:10 -0500

    Remove tsPaths plugin for vite

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,16 @@
-import tsPaths from 'vite-tsconfig-paths';
+import { resolve } from 'node:path';
+
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [tsPaths()],
   test: {
     include: ['tests/**/*.{spec,test}.?(c|m)[jt]s?(x)'],
     globalSetup: ['tests/global-setup.ts'],
+  },
+  resolve: {
+    alias: {
+      '~': resolve('src'),
+      '~tests': resolve('tests'),
+    },
   },
 });
